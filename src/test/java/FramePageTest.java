8a450d83455550a851474c74307a00a3212dad63
Zhanar <sydygaliev12@gmail.com>
2024-06-25 19:28:08 +0600

    Update FramePageTest.java

--- src/test/java/FramePageTest.java
+++ src/test/java/FramePageTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class FramePageTest extends BaseTest{
-    @Test(description = "Verify that driver can switch to another frame")
+   @Test(groups = {"UI","Smoke","NBW-18"}, description = "Verify that driver can switch to another frame")
     public void doubleClickTest(){
         browserHelper.open(ConfigReader.getValue("baseURL") + EndPointsEnum.FRAMES.getEndpoint());
         iframeHelper.switchToFrame("frame1");
