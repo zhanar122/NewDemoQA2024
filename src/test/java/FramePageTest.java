bd55dbfebf0d914e7e7a4834873e988e06860b40
Zhanar <sydygaliev12@gmail.com>
2024-07-30 19:31:01 +0600

    docker setup

--- src/test/java/FramePageTest.java
+++ src/test/java/FramePageTest.java
@@ -1,10 +1,15 @@
 import com.demoqa.enums.EndPointsEnum;
 import com.demoqa.utils.ConfigReader;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class FramePageTest extends BaseTest{
-    @Test(description = "Verify that driver can switch to another frame")
+
+    @Test(groups = {"UI","Smoke","NBW-18"}, description = "Verify that driver can switch to another frame")
+    @Link("https://demoqa.com/")
+    @Owner("Janar")
     public void doubleClickTest(){
         browserHelper.open(ConfigReader.getValue("baseURL") + EndPointsEnum.FRAMES.getEndpoint());
         iframeHelper.switchToFrame("frame1");
@@ -12,4 +17,5 @@ public class FramePageTest extends BaseTest{
         iframeHelper.switchToParentFrame();
         System.out.println(driver.findElement(By.tagName("h1")).getText());
     }
+
 }
